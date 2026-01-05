0b1f8e749b4e97f3bd0013c1a3d889a7ffd9ad63
Shumaila Riaz <shumailariaz215@gmail.com>
2026-01-05 15:59:02 +0500

    errors handled

--- models/User.js
+++ models/User.js
@@ -1,66 +1,68 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        required: [true, 'Please provide name'],
-        maxlength: 50,
-        minlength: 3,
+      type: String,
+      required: [true, "Please provide name"],
+      maxlength: 50,
+      minlength: 3,
     },
     lastName: {
-        type: String,
-        required: [true, 'Please provide name'],
-        maxlength: 50,
-        minlength: 3,
+      type: String,
+      required: [true, "Please provide name"],
+      maxlength: 50,
+      minlength: 3,
     },
     email: {
-        type: String,
-        required: [true, 'Please provide email'],
-        match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            'Please provide a valid email',
-        ],
-        unique: true,
+      type: String,
+      required: [true, "Please provide email"],
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please provide a valid email",
+      ],
+      unique: true,
     },
     password: {
-        type: String,
-        required: [true, 'Please provide password'],
-        minlength: 6
+      type: String,
+      required: [true, "Please provide password"],
+      minlength: 6,
     },
-     role:{
-        type:String,
-        enum: ['admin', 'manager'],
-        default: 'driver'
-    }
-},{
-    timestamps:true
-})
+    role: {
+      type: String,
+      enum: ["admin", "manager", "driver"],
+      default: "driver",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 UserSchema.pre("save", async function () {
-    if (!this.isModified('password')) return;
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt)
-})
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 // instance method to create JWT
 UserSchema.methods.createJWT = function () {
-    return jwt.sign(
-        {
-            userId: this._id,
-            firstName: this.firstName,
-            lastName: this.lastName,
-            role: this.role
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: process.env.JWT_LIFETIME }
-    )
-}
-
+  return jwt.sign(
+    {
+      userId: this._id,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      role: this.role,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
+};
 
 UserSchema.methods.comparePassword = async function (candidatePassword) {
-    const isMatch = await bcrypt.compare(candidatePassword, this.password);
-    return isMatch;
-}
-module.exports = mongoose.model('User', UserSchema)
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+module.exports = mongoose.model("User", UserSchema);
