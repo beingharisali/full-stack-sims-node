673c5b986673c57f4fd5a80c459c1a48069c53dc
Bareera Nawaz <bareeranawaz1043@gmail.com>
2026-01-08 23:04:19 +0500

    create inventory controllers created

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -1,5 +1,6 @@
 const inventoryModel = require("../models/inventory");
 
+// SIMS-NODE-13
 const getInventory = async (req, res) => {
     try {
         const inventory = await inventoryModel.find({});
@@ -17,6 +18,35 @@ const getInventory = async (req, res) => {
     }
 };
 
+// SIMS-NODE-14
+const createInventory = async (req, res) => {
+    try {
+        const setInventory = new inventoryModel({
+            productName: req.body.productName,
+            description: req.body.description,
+            category: req.body.category,
+            price: req.body.price,
+            supplier: req.body.supplier,
+            quantity: req.body.quantity || 0,
+        });
+
+        await setInventory.save();
+
+        res.status(201).json({
+            success: true,
+            message: "Inventory created successfully",
+            data: setInventory,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Unable to create inventory",
+            error: error.message,
+        });
+    }
+};
+
 module.exports = {
     getInventory,
+    createInventory,
 };
