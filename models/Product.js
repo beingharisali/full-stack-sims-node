0ee528ff411f85e18f94c528722a793784f25366
Shumaila Riaz <shumailariaz215@gmail.com>
2026-01-08 16:07:29 +0500

    validations added in product model

--- models/Product.js
+++ models/Product.js
@@ -4,21 +4,39 @@ const productSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      required: [true, "Please provide name"],
+      maxlength: 100,
+      minlength: [2, "Product name must be at least 2 characters"],
+      trim: true,
     },
     description: {
       type: String,
+      required: [true, "Please provide description"],
+      maxlength: 150,
+      minlength: 3,
+      trim: true,
     },
     category: {
       type: String,
+      trim: true,
     },
     price: {
       type: Number,
+      required: [true, "Product price is required"],
+      min: [0, "Price cannot be negative"],
     },
     supplier: {
       type: String,
+      required: [true, "Please provide supplier name"],
+      maxlength: 50,
+      minlength: 3,
+      trim: true,
     },
     stock: {
       type: Number,
+      default: 0,
+      required: [true, "Stock quantity is required"],
+      min: [0, "Stock quantity cannot be in negative numbers"],
     },
   },
   {
