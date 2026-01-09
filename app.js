e96fe02aa0321e50c8105522e702301bda3091fe
Shumaila Riaz <shumailariaz215@gmail.com>
2026-01-09 17:21:04 +0500

    routes added

--- app.js
+++ app.js
@@ -2,40 +2,48 @@ require("dotenv").config({ path: "./.env" });
 
 const express = require("express");
 const cors = require("cors");
+<<<<<<< HEAD
 const app = express();
 const productRouter = require("./routes/products");
 
 // extra security packages
+=======
+>>>>>>> bc36db28d424124dc822fad65393c20624f0f4ef
 const helmet = require("helmet");
 const rateLimiter = require("express-rate-limit");
 
-// connectDB
-const connectDB = require("./db/connect");
-
-// routers
-const authRouter = require("./routes/auth");
-
-// error handler
-const notFoundMiddleware = require("./middleware/not-found");
-const errorHandlerMiddleware = require("./middleware/error-handler");
-const auth = require("./middleware/authentication");
+const app = express();
 
 app.use(cors());
-app.use(express.json());
 app.use(
   rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
   })
 );
 app.use(helmet());
+<<<<<<< HEAD
 app.use(express.json());
 // routes
+=======
+
+const inventoryRoutes = require("./routes/inventory");
+const authRouter = require("./routes/auth");
+
+app.use(express.json());
+
+app.use("/api/v1/", inventoryRoutes);
+>>>>>>> bc36db28d424124dc822fad65393c20624f0f4ef
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/", productRouter);
 
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
+
+const connectDB = require("./db/connect");
 
 const port = process.env.PORT || 5000;
 
