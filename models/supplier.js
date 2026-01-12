b1f1e348c6d83d994fbd1573e0deb3c089b1813f
Shumaila Riaz <shumailariaz215@gmail.com>
2026-01-12 17:15:28 +0500

    validations added

--- models/supplier.js
+++ models/supplier.js
@@ -1,20 +1,51 @@
 const mongoose = require("mongoose");
 
-const supplierSchema = new mongoose.Schema(
-  {
-    supplierGroup: String,
+const supplierSchema = new mongoose.Schema({
+  supplierGroup: {
+    type: String,
+    required: [true, "Please provide supplier name"],
+    maxlength: 100,
+    minlength: [2, "Product name must be at least 2 characters"],
+    trim: true,
+    index: true,
   },
-  {
-    name: String,
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    maxlength: 150,
+    minlength: 3,
+    trim: true,
   },
-  {
-    contactNumber: Number,
+  contactNumber: {
+    type: Number,
+    required: [true, "Contact number is required"],
+    minlength: 11,
+    maxlength: 15,
   },
-  {
-    category: String,
+  category: {
+    type: String,
+    trim: true,
+    required: [true, "Please provide a category"],
+    enum: [
+      "mobile",
+      "laptop",
+      "headphones",
+      "tablet",
+      "television",
+      "camera",
+      "headphones",
+      "smartwatch",
+      "accessories",
+      "home-appliances",
+    ],
+    trim: true,
   },
-  {
-    status: String,
-  }
-);
+  status: {
+    type: String,
+    required: [true, "Please provide description"],
+    maxlength: 150,
+    minlength: 3,
+    trim: true,
+  },
+});
 module.exports = mongoose.model("Supplier", supplierSchema);
