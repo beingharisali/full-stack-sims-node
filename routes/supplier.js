8a9e9e88cb7d13e11c6576824f8e3f7bcda35c7a
Shumaila Riaz <shumailariaz215@gmail.com>
2026-01-13 12:05:00 +0500

    update supplier controllers

--- routes/supplier.js
+++ routes/supplier.js
@@ -4,8 +4,12 @@ const {
   createSupplier,
   getSupplier,
   getSingleSupplier,
+  updateSupplier,
 } = require("../controllers/supplier");
+
 router.post("/create", createSupplier);
 router.get("/get", getSupplier);
 router.get("/get/:id", getSingleSupplier);
+router.put("/update/:id", updateSupplier);
+
 module.exports = router;
