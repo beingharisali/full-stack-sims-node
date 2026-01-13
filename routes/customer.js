59b800c223d32322c052b89cc275053c5511bbd7
Shumaila Riaz <shumailariaz215@gmail.com>
2026-01-13 17:55:01 +0500

    customer routes added

--- routes/customer.js
+++ routes/customer.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { createCustomer } = require("../controllers/customer");
+const {
+  createCustomer,
+  getCustomer,
+  getSingleCustomer,
+  updateCustomer,
+  deleteCustomer,
+} = require("../controllers/customer");
 
 router.post("/create", createCustomer);
+router.get("/get", getCustomer);
+router.get("/get/:id", getSingleCustomer);
+router.put("/update/:id", updateCustomer);
+router.delete("/delete/:id", deleteCustomer);
 module.exports = router;
