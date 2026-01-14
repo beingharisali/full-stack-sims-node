f9fee2a8b303394f9f556f5a87b63e322f3bfcf0
Ansa-dev710 <ansaasgar710@gmail.com>
2026-01-14 14:25:45 +0500

    created saler in controller

---
+++ controllers/saler.js
@@ -0,0 +1,28 @@
+const supplierModel = require("../models/supplier");
+const createSupplier = async (req, res) => {
+  try {
+    const createSupplier = new supplierModel({
+      supplierGroup: req.body.supplierGroup,
+      name: req.body.name,
+      contactNumber: req.body.contactNumber,
+      category: req.body.category,
+      status: req.body.status,
+    });
+    await createSupplier.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Supplier has been created successfully",
+      data: createSupplier,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Unable to create supplier",
+      data: error.message,
+    });
+  }
+};
+module.exports = {
+  createSupplier,
+};
