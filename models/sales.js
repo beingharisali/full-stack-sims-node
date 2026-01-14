c2c36042c3dd1e51c7f1b8dd58a526779528aa12
Ansa-dev710 <ansaasgar710@gmail.com>
2026-01-14 14:12:05 +0500

    validations added in saler

---
+++ models/sales.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const salerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    maxlength: 50,
+    minlength: 3,
+    trim: true,
+  },
+  contactNumber: {
+    type: String,
+    required: [true, "Contact number is required"],
+    minlength: 11,
+    maxlength: 15,
+  },
+  category: {
+    type: String,
+    trim: true,
+    required: [true, "Please provide a category"],
+    enum: [
+      "mobile",
+      "laptop",
+      "headphones",
+      "tablet",
+      "televison",
+      "camera",
+      "headphones",
+      "smartwatch",
+      "accessories",
+      "home-appliances",
+    ],
+  },
+  status: {
+    type: String,
+    required: [true, "Please provide description"],
+    maxlength: 10,
+    minlength: 3,
+    trim: true,
+  },
+  orderitems: {
+    type: Number,
+    required: [true, "Please provide description"],
+    trim: true,
+  },
+});
+module.exports = mongoose.model("Saler", salerSchema);
