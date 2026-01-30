397b5d7e6aa663de50a3af0e6eccf0c8f72505bc
Shumaila Riaz <shumailariaz215@gmail.com>
2026-01-30 15:23:43 +0500

    backend api corrected

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,26 +1,37 @@
 const Product = require("../models/Product");
 const Sale = require("../models/saler");
 const Invoice = require("../models/invoice");
+const Customer = require("../models/customer");
+const Supplier = require("../models/supplier");
+const User = require("../models/User");
 
 exports.getDashboardStats = async (req, res) => {
   try {
     const totalProducts = await Product.countDocuments();
 
-    const totalStock = await Product.aggregate([
+    const totalStockAgg = await Product.aggregate([
       { $group: { _id: null, total: { $sum: "$stock" } } },
     ]);
+    const totalStock = totalStockAgg[0]?.total || 0;
 
-    const totalSales = await Sale.aggregate([
+    const totalSalesAgg = await Sale.aggregate([
       { $group: { _id: null, total: { $sum: "$totalAmount" } } },
     ]);
+    const totalSales = totalSalesAgg[0]?.total || 0;
 
-    const invoices = await Invoice.countDocuments();
+    const totalInvoices = await Invoice.countDocuments();
+    const totalCustomers = await Customer.countDocuments();
+    const totalSuppliers = await Supplier.countDocuments();
+    const totalUsers = await User.countDocuments();
 
     res.status(200).json({
       totalProducts,
-      totalStock: totalStock[0]?.total || 0,
-      totalSales: totalSales[0]?.total || 0,
-      invoices,
+      totalStock,
+      totalSales,
+      totalInvoices,
+      totalCustomers,
+      totalSuppliers,
+      totalUsers,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
