a1c43ee067230e042f5d0c46129e183e822eb472
Shumaila Riaz <shumailariaz215@gmail.com>
2026-02-13 13:59:53 +0500

    controllers updated

--- models/saler.js
+++ models/saler.js
@@ -1,47 +1,44 @@
 const mongoose = require("mongoose");
 
-const salerSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide name"],
-    maxlength: 50,
-    minlength: 3,
-    trim: true,
+const salerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide name"],
+      maxlength: 50,
+      minlength: 3,
+      trim: true,
+    },
+
+    contactNumber: {
+      type: String,
+      required: [true, "Contact number is required"],
+      minlength: 11,
+      maxlength: 15,
+    },
+
+    // 🔥 Category now free text
+    category: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide a category"],
+    },
+
+    status: {
+      type: String,
+      enum: ["active", "inactive", "suspended"],
+      default: "active",
+      required: true,
+    },
+
+    orderitems: {
+      type: Number,
+      required: [true, "Please provide order items count"],
+      min: 0,
+      default: 0,
+    },
   },
-  contactNumber: {
-    type: String,
-    required: [true, "Contact number is required"],
-    minlength: 11,
-    maxlength: 15,
-  },
-  category: {
-    type: String,
-    trim: true,
-    required: [true, "Please provide a category"],
-    enum: [
-      "mobile",
-      "laptop",
-      "headphones",
-      "tablet",
-      "televison",
-      "camera",
-      "smartwatch",
-      "accessories",
-      "home-appliances",
-    ],
-  },
-  status: {
-    type: String,
-    required: [true, "Please provide description"],
-    maxlength: 10,
-    minlength: 3,
-    trim: true,
-  },
-  orderitems: {
-    type: Number,
-    required: [true, "Please provide order items count"],
-    min: 0,
-  },
-});
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("Saler", salerSchema);
